76043fbf4f252fc196fad2f08fff157a6c19b0c7
nianman <iman0orujov@gmail.com>
2020-09-05 15:28:11 +0400

    Updates

---
+++ precache-manifest.866ad33d9b79cb567aa4770feb271e48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c75012f01c560d52b853474eb28361",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "f51327eb69673dca1eaa",
+    "url": "/note-app/static/js/2.795a0560.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/note-app/static/js/2.795a0560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8504e506a14e2d16d86d",
+    "url": "/note-app/static/js/main.b78415ba.chunk.js"
+  },
+  {
+    "revision": "f0c317bda8eb215732d1",
+    "url": "/note-app/static/js/runtime-main.52abe802.js"
+  }
+]);
